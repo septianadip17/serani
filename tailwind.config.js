878199f449df7d6f17e61c87c53b0bfa00843169
Septian Adi Pratama <septianadip7998@gmail.com>
2025-07-30 22:41:05 +0700

    refactor: clean up HTML structure and improve CSS formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [ `./index.html`, `./src/**/*.{js,ts,jsx,tsx}`, ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blush: "#ffe4e6",
+        pinky: "#fbcfe8",
+        rose: "#fda4af",
+      },
+      fontFamily: {
+        script: ['"Dancing Script"', "cursive"],
+        body: ['"Poppins"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
