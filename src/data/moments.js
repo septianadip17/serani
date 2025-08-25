b5dce4fb8618c305ebb648df26901e836e56f234
Septian Adi Pratama <septianadip7998@gmail.com>
2025-08-26 01:13:02 +0700

    data added

---
+++ src/data/moments.js
@@ -0,0 +1,91 @@
+const moments = [
+  { date: "8 Juni (Sabtu)", title: "Awal Kenal", emoji: "💌" },
+  {
+    date: "13 Juni (Kamis)",
+    title: "Pertemuan 1 – Tomorro Coffee setelah dia pulang kerja",
+    emoji: "☕️",
+  },
+  {
+    date: "17 Juni (Senin)",
+    title: "Pertemuan 2 – Jemput, makan ramen, dia masih sakit",
+    emoji: "🍜🤒",
+  },
+  {
+    date: "30 Juni (Minggu)",
+    title: "Pertemuan 3 – Nonton bioskop, selfie bareng pertama",
+    emoji: "🎬🤳",
+  },
+  {
+    date: "1 Juli (Senin)",
+    title: "Pertemuan 4 – Jogging GBK, kasih bunga pertama",
+    emoji: "🏃‍♀️🌸",
+  },
+  {
+    date: "3 Juli (Rabu)",
+    title: "Pertemuan 5 – Jemput, makan mie Bangladesh (2 porsi!)",
+    emoji: "🍜🇧🇩",
+  },
+  {
+    date: "7 Juli (Minggu)",
+    title: "Pertemuan 6 – Kuncit: Miniso, Puma, Watson, beli skincare",
+    emoji: "🛍️💄",
+  },
+  {
+    date: "8 Juli (Senin)",
+    title: "Pertemuan 7 – Antar ke kantor, dia bawain bekal",
+    emoji: "🍱💖",
+  },
+  {
+    date: "9 Juli (Selasa)",
+    title: "Pertemuan 8 – Ketemu dan dia tampil cantik banget",
+    emoji: "😍",
+  },
+  {
+    date: "12 Juli (Jumat)",
+    title: "Pertemuan 9 – Ke PRJ, dia ngambek pas pulang",
+    emoji: "🎡😅",
+  },
+  {
+    date: "14 Juli (Minggu)",
+    title: "Pertemuan 10 – Bawain makanan (katanya ga enak), dia kasih sarapan",
+    emoji: "🍳🍽️",
+  },
+  {
+    date: "15 Juli (Senin)",
+    title: "Pertemuan 11 – Jemput, makan nasgor UMKM di Tebet",
+    emoji: "🍚",
+  },
+  {
+    date: "16 Juli (Selasa)",
+    title: "Pertemuan 12 – Makan mie Gacoan dan cimol Bojot",
+    emoji: "🍜🍢",
+  },
+  {
+    date: "17 Juli (Rabu)",
+    title: "Pertemuan 13 – Makan di foodcourt Kuncit, ke Tomorro Coffee",
+    emoji: "🍴☕️",
+  },
+  {
+    date: "20 Juli (Sabtu)",
+    title: "Pertemuan 14 – CFD, pisang ijo, naik KRL dan MRT, outfit check",
+    emoji: "🚶‍♂️🚆✨",
+  },
+  {
+    date: "27 Juli (Sabtu)",
+    title: "Pertemuan 15 – Nonton di Kelapa Gading, naik TransJakarta",
+    emoji: "🎥🚌",
+  },
+  {
+    date: "28 Juli (Minggu)",
+    title: "Pertemuan 16 – Jemput dan nail art bareng",
+    emoji: "💅💖",
+  },
+  {
+    date: "31 Juli (Rabu)",
+    title:
+      "Pertemuan 17 – Belanja bareng, makan Ricis, ngobrol tentang perasaan",
+    emoji: "🛒🍗❤️",
+  },
+];
+
+export default moments;
